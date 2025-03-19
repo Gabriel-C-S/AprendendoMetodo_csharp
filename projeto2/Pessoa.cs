a365398d0f42b75153f471979cc51f6a3fd696b1
unknown <gabrielclayton961@gmail.com>
2025-03-18 22:21:26 -0300

    Projeto 18-03-2025

---
+++ projeto2/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto2
+{
+    internal class Pessoa
+    {
+        public double altura;
+        public double peso;
+        public string etinia;
+        public string nome;
+        public string idade;
+    }
+}
---
+++ projeto2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using projeto2;
+
+namespace Metodo
+{
+    internal class program
+    {
+        static void Main(string[] arg) //Método
+        {
+        Pessoa aluno = new Pessoa();
+        Pessoa aluno1 = new Pessoa();
+        aluno.nome = "Josue Correa Pinto Lopes Junior Filho Neto";
+        aluno1.nome = "Firmino Silva Menezes Santos da Costa";
+        aluno.peso = 90;
+        aluno1.peso = 85;
+
+        Console.WriteLine("Aluno {0}", aluno.nome);
+        //Console.WriteLine("Aluno {3}", aluno1.nome);
+        }
+    }
+};
